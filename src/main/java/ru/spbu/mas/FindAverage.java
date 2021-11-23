caca9879e98566fc8ae6d2f50f0bc6d365228f4e
CunningPie <Makarikhin.P.A@gmail.com>
2021-11-23 12:50:31 +0300

    Fixed message sending to center part

--- src/main/java/ru/spbu/mas/FindAverage.java
+++ src/main/java/ru/spbu/mas/FindAverage.java
@@ -5,12 +5,13 @@ import jade.core.behaviours.CyclicBehaviour;
 import jade.lang.acl.ACLMessage;
 
 import java.util.Arrays;
+import java.util.Objects;
 
 public class FindAverage extends CyclicBehaviour {
     private final DefaultAgent agent;
     private boolean firstSend = true;
     private boolean finishProcess = false;
-    private boolean isInformed = false;
+    private boolean isInforming = false;
     private boolean needToSend = true;
 
     FindAverage(DefaultAgent agent) {
@@ -53,7 +54,7 @@ public class FindAverage extends CyclicBehaviour {
         {
             //System.out.println("Agent #" + this.agent.getLocalName() + " received message: " + msg.getContent() +" from Agent #" + msg.getSender().getLocalName());
             // Проверка сообщения на продолжение подсчета
-            if (msg.getPerformative() == ACLMessage.PROPOSE) {
+            if (msg.getPerformative() == ACLMessage.PROPOSE && !finishProcess) {
                 String[] splited = msg.getContent().split(" ");
 
                 for (int i = 0; i < agent.numbers.length; i++) {
@@ -71,34 +72,46 @@ public class FindAverage extends CyclicBehaviour {
                     String newContent = arrToString(agent.numbers);
                     dataMessage.setContent(newContent);
 
+                    StringBuilder receivers = new StringBuilder();
+
                     for (int j = 0; j < agent.linkedAgents.size(); j++)
-                        dataMessage.addReceiver(new AID(agent.linkedAgents.get(j), AID.ISLOCALNAME));
+                        if (!Objects.equals(agent.linkedAgents.get(j), msg.getSender().getLocalName())) {
+                            dataMessage.addReceiver(new AID(agent.linkedAgents.get(j), AID.ISLOCALNAME));
+                            receivers.append(agent.linkedAgents.get(j)).append(" ");
+                        }
 
                     agent.send(dataMessage);
-                    System.out.println("Agent #" + this.agent.getLocalName() + " send: " + newContent + " to Agents: " + String.join(" ", agent.linkedAgents));
+                    System.out.println("Agent #" + this.agent.getLocalName() + " send: " + newContent + " to Agents: " + receivers);
                     needToSend = false;
                 }
 
                 if (finishProcess)
                 {
                     int sum = Arrays.stream(agent.numbers).sum();
-                    System.out.println("Agent #" + this.agent.getLocalName() + " calculate average value: " + (double)sum / agent.numbers.length);
+                    //System.out.println("Agent #" + this.agent.getLocalName() + " calculate average value: " + (double)sum / agent.numbers.length);
+
+                    if (Objects.equals(this.agent.getLocalName(), "0"))
+                        System.out.println("Agent #" + this.agent.getLocalName() + " send message to center with value: " + (double)sum / agent.numbers.length);
 
                     ACLMessage informMessage = new ACLMessage(ACLMessage.INFORM);
-                    String newContent = "Finish process";
+                    String newContent = String.valueOf((double)sum / agent.numbers.length);
                     informMessage.setContent(newContent);
 
                     for (int j = 0; j < agent.linkedAgents.size(); j++)
                         informMessage.addReceiver(new AID(agent.linkedAgents.get(j), AID.ISLOCALNAME));
 
-                    isInformed = true;
+                    isInforming = true;
+                    finishProcess = true;
                     agent.send(informMessage);
                     agent.doDelete();
                 }
             }
             // если полученное сообщение - информирование о конце работы, и текущий агент еще не предупреждал связанных агентов о завершении процесса
-            else if (msg.getPerformative() == ACLMessage.INFORM && !isInformed)
+            else if (msg.getPerformative() == ACLMessage.INFORM && !isInforming)
             {
+                if (Objects.equals(this.agent.getLocalName(), "0"))
+                    System.out.println("Agent #" + this.agent.getLocalName() + " send message to center with value: " + msg.getContent());
+
                 System.out.println("Agent #" + this.agent.getLocalName() + " finished work.");
 
                 ACLMessage dataMessage = new ACLMessage(ACLMessage.INFORM);
@@ -106,8 +119,11 @@ public class FindAverage extends CyclicBehaviour {
                 dataMessage.setContent(newContent);
 
                 for (int j = 0; j < agent.linkedAgents.size(); j++)
-                    dataMessage.addReceiver(new AID(agent.linkedAgents.get(j), AID.ISLOCALNAME));
-                isInformed = true;
+                    if (!Objects.equals(agent.linkedAgents.get(j), msg.getSender().getLocalName())) {
+                        dataMessage.addReceiver(new AID(agent.linkedAgents.get(j), AID.ISLOCALNAME));
+                    }
+
+                isInforming = true;
                 agent.send(dataMessage);
                 agent.doDelete();
             }
